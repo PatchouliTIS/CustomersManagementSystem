c6c0c91844ff27727a02a9eddaf5617808c153fd
Maestera <623894679@qq.com>
2022-06-27 15:30:46 +0800

    customer_JavaTest

---
+++ FirstIDEATest/src/com/rangerRGT/MultiNum/MultiNum.java
@@ -0,0 +1,19 @@
+package com.rangerRGT.MultiNum;
+
+import java.util.*;
+
+public class MultiNum {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int x ;
+        int y ;
+        System.out.println("Please input x:");
+        x = scan.nextInt();
+
+        System.out.println("Please input y:");
+        y = scan.nextInt();
+
+        System.out.println("Final value is :" + (x * y));
+
+    }
+}
